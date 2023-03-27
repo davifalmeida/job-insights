d01471a27850de9466854bd891ecfeb65642d531
Davi <davifalmeida@gmail.com>
2023-03-27 14:11:46 -0300

    test: brazilian_jobs tests

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+
+    for job in jobs:
+        for key, _value in job.items():
+            assert key in ["title", "salary", "type"]
