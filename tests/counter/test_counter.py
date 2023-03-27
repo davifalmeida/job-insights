7b4720b19934ccfff218756fe629e10ba6841cf0
Davi <davifalmeida@gmail.com>
2023-03-27 13:54:50 -0300

    test: add test_couter

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,13 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+import pytest
 
 
 def test_counter():
-    pass
+
+    path = "data/jobs.csv"
+    word = "python"
+    expected_count = 1639
+    assert count_ocurrences(path, word) == expected_count
+
+    with pytest.raises(FileNotFoundError):
+        count_ocurrences("path/does/not/exist.csv", "python")
