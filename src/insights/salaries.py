9f70c6bd69d2d72d8cb2e96d807639ce3612f644
Davi <davifalmeida@gmail.com>
2023-03-27 12:10:03 -0300

    feat: Implementation of function matches_salary_range in salaries.py

--- src/insights/salaries.py
+++ src/insights/salaries.py
@@ -32,29 +32,14 @@ def get_min_salary(path: str) -> int:
 
 
 def matches_salary_range(job: Dict, salary: Union[int, str]) -> bool:
-    """Checks if a given salary is in the salary range of a given job
+    if ("min_salary" not in job or "max_salary" not in job or
+            (not isinstance(job["min_salary"], (int, str)) or
+             not isinstance(job["max_salary"], (int, str))) or
+            int(job["min_salary"]) > int(job["max_salary"]) or
+            not isinstance(salary, (int, str))):
+        raise ValueError("Invalid job or salary information")
 
-    Parameters
-    ----------
-    job : dict
-        The job with `min_salary` and `max_salary` keys
-    salary : int
-        The salary to check if matches with salary range of the job
-
-    Returns
-    -------
-    bool
-        True if the salary is in the salary range of the job, False otherwise
-
-    Raises
-    ------
-    ValueError
-        If `job["min_salary"]` or `job["max_salary"]` doesn't exists
-        If `job["min_salary"]` or `job["max_salary"]` aren't valid integers
-        If `job["min_salary"]` is greather than `job["max_salary"]`
-        If `salary` isn't a valid integer
-    """
-    raise NotImplementedError
+    return int(job["min_salary"]) <= int(salary) <= int(job["max_salary"])
 
 
 def filter_by_salary_range(
