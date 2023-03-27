b10c1489431bab4597b5ee08bbc1d76324b83c53
Davi <davifalmeida@gmail.com>
2023-03-27 10:30:09 -0300

    feat: implementation of the get _unique_industries function in the industries.py

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,17 @@
 from typing import List, Dict
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
+    data = read(path)
+    unique_industries = set()
 
-    Must call `read`
+    for row in data:
+        industry = row.get('industry', '').strip()
+        if industry:
+            unique_industries.add(industry)
 
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    return list(unique_industries)
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
